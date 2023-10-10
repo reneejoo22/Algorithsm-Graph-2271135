c8570892820e22895adbba43446239e452e2832b
reneejoo22 <renee.joo03@gmail.com>
2023-10-11 00:53:51 +0900

    dfs

--- graph.2271135.c
+++ graph.2271135.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define True 1
+#define False 0
 #define max_vertices 50
-
-typedef struct GraphNode {
-	int vertex; //정점의 갯수
-	struct GraphNode* link;	//인접행렬의 이름
-}GraphNode;
 
 typedef struct GraphType {
 	int vertexNum; //정점의 갯수
-	GraphNode* adj_list[max_vertices];	//인접행렬의 이름
+	int adj_mat[max_vertices][max_vertices];	//인접행렬의 이름
 }GraphType;
 
+int visited[max_vertices];
 
 //그래프 초기화
 void init(GraphType* g) {
 
-	int r;
+	int r, c;
 
 	g->vertexNum = 0;	//멤버변수_정점의 갯수 0으로 초기화
 	for (r = 0; r < max_vertices; r++) {	//멤버변수_인접행렬 0으로 초기화 
-			g->adj_list[r] = 0;
+		for (c = 0; c < max_vertices; c++) {
+			g->adj_mat[r][c] = 0;
+		}
 	}
 
 }
@@ -39,41 +39,32 @@ void insertVertex(GraphType* g, int v) {
 }
 
 //간선 삽입
-void insert_edge(GraphType* g, int u, int v) {
+void insert_edge(GraphType* g, int start, int end) {
 
-	GraphNode* node;
-
-	if (u >= g->vertexNum || v >= g->vertexNum) {	//start <= vertexNum <= end 
+	if (start >= g->vertexNum || end >= g->vertexNum) {	//start <= vertexNum <= end 
 		fprintf(stderr, "그래프 정점 번호 오류");	//vertexNum은 start랑 end보다 같지 않고 커야됨
 		return;
 	}
-
-	node = (GraphNode*)malloc(sizeof(GraphNode));
-	
-	if (node) {
-		node->vertex = v;	//정점 칸에 있는 수
-		node->link = g->adj_list[u];
-		g->adj_list[u] = node;
-	}else printf("\n노드가 동적 할당되지 않음\n");
+	g->adj_mat[start][end] = 1;
+	g->adj_mat[end][start] = 1;
 }
 
+//인접 행렬로 표현된 그래프의 깊이 우선 탐색
+void dfs_mat(GraphType* g, int v) {
 
-//인접 리스트 출력
-void print_adjList(GraphType* g) {
+	int w;
+	visited[v] = True;
+	printf("정점 %d -> ", v);
 
-	for (int i = 0; i < g->vertexNum; i++) {
-		GraphNode* p = g->adj_list[i];
-		printf("정점%d에 있는 인접 리스트 ", i);
-		
-		while (p != NULL) {
-			printf("-> %d ", p->vertex);
-			p = p->link;
-		}
-		printf("\n");
+	for (w = 0; w < g->vertexNum; w++) {
+		if (g->adj_mat[v][w] && !visited[w])	//visited가 true이고 adj_mat에 값이 잇을때
+			//v와 정점 w 사이에 간선이 있고, 정점 w가 방문되지 않았다면, 해당 조건이 참
+			dfs_mat(g, w);
 	}
+
 }
 
-int main() {
+int main(void) {
 
 	GraphType* myGraph;
 	myGraph = (GraphType*)malloc(sizeof(GraphType));
@@ -82,18 +73,15 @@ int main() {
 	for (int i = 0; i < 4; i++) 	//4개의 정점을 삽입할 것
 		insertVertex(myGraph, i);
 
-	insert_edge(myGraph, 0, 1);		//0의 인접 리스트에 1이 추가
-	insert_edge(myGraph, 1, 0);		//1의 인접 리스트에 0이 추가
-	insert_edge(myGraph, 0, 2);		//0의 인접 리스트에 2이 추가
-	insert_edge(myGraph, 2, 0);		//2의 인접 리스트에 0이 추가
-	insert_edge(myGraph, 0, 3);		//0의 인접 리스트에 3이 추가
-	insert_edge(myGraph, 3, 0);
-	insert_edge(myGraph, 1, 2);
-	insert_edge(myGraph, 2, 1);
+	insert_edge(myGraph, 0, 1);	
+	insert_edge(myGraph, 0, 2);		
+	insert_edge(myGraph, 0, 3);		
+	insert_edge(myGraph, 1, 2);		
 	insert_edge(myGraph, 2, 3);
-	insert_edge(myGraph, 3, 2);
 
-	print_adjList(myGraph);
+	printf("깊이 우선 탐색\n");
+	dfs_mat(myGraph, 0);
+	printf("\n");
 
 	free(myGraph);
 }
